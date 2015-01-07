c97c1abfeeab2bb2d8949a8f2936990acf6e258a
Kathlyn Miller <katkit101999@hotmail.com>
2015-01-06 20:42:44 -0800

    i made three classes with names , variables , functions

---
+++ notes.php
@@ -0,0 +1,57 @@
+<?php
+
+class Katty {
+
+	$katty1 = new Katty();
+    $katty2 = new Katty();
+    $katty3 = new Katty();
+
+    public $name = "Kathlyn";
+    public $age = "15";
+    public $school = "Pasadena High School";
+
+    $katty1 = new Katty();
+    print $katty1 > name;
+
+    public function Katty( $name, $age, $school) {
+      // stuff
+    }
+}
+
+class Cream {
+
+	$cream1 = new Cream();
+    $cream2 = new Cream();
+    $cream3 = new Cream();
+
+    public $vanilla = "good";
+    public $chocolate = "great";
+    public $mint = "okay";
+
+    $cream1 = new Cream();
+    print $cream1 > name;
+
+    public function Cream( $vanilla, $chocolate, $mint) {
+      // stuff
+    }
+}
+
+class Colors {
+
+	$color1 = new Colors();
+    $color2 = new Colors();
+    $color3 = new Colors();
+
+    public $bright = "yellow";
+    public $dark = "black";
+    public $favorite = "blue";
+
+    $color1 = new Colors();
+    print $color1 > name;
+
+    public function Colors( $bright, $dark, $favorite) {
+      // stuff
+    }
+}
+
+?>
