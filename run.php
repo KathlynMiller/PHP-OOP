3f85abdd32920fd23a228f34271b66b95a64efb1
Kathlyn Miller <katkit101999@hotmail.com>
2015-01-08 20:41:31 -0800

    using if else statements with is_integer

---
+++ run.php
@@ -0,0 +1,28 @@
+<?php
+
+if (is_integer(5) > 8)
+{
+	echo "good";
+}
+else {
+	echo "anything" . "<br />";
+}
+
+
+if (is_integer(18) > 31)
+{
+	echo "YAAAAAASSSSSSSSSS";
+}
+else {
+	echo "UM NOOOOOOO";
+}
+
+if (is_integer(2222) > 0)
+{
+	echo "you so fab ;) ";
+}
+else {
+	echo "nahh u so ugly";
+}
+
+?>
